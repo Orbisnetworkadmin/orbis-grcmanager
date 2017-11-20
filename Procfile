7913c19c2f0da8c7e89a7625450cdf6b0c51fa63
Orbis network <32267493+Orbisnetworkadmin@users.noreply.github.com>
2017-11-20 12:58:59 -0400

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/uberjar/orbis_grcmanager.jar clojure.main -m orbis_grcmanager.core
---
+++ profiles.clj
@@ -0,0 +1,5 @@
+{:profiles/dev
+ {:env
+  {:database-url "jdbc:postgresql://localhost/postgres?user=orbis&password=orbis"
+  ;;optional, will use internal table otherwise
+  }}}
