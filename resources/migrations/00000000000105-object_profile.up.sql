34bf1e02ddd21ab7c637cd1909c4d3c98b07c72d
Fabian Herrera <fabianherrera@gmail.com>
2017-11-21 17:55:00 -0400

    Cambios Migrate DB

--- resources/migrations/00000000000105-object_profile.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE objects_profile
-(
- id_object                      BIGINT    NOT NULL REFERENCES objects (id_object) DEFERRABLE,
- id_profile                     BIGINT    NOT NULL REFERENCES profile (id_profile) DEFERRABLE,
- grant_object_profile           VARCHAR(255)
-);
-
---;;
--- CREATE UNIQUE INDEX idx_support_issue_id_tag_id ON support_issues_tags(support_issue_id, tag_id);
