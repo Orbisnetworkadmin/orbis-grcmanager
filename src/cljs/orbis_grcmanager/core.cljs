418bfa56d4084392ac69e07e4174f786e98d471e
ingsissam <33227372+ingsissam@users.noreply.github.com>
2017-11-22 10:51:25 -0400

    anadir re-firsk

--- src/cljs/orbis_grcmanager/core.cljs
+++ src/cljs/orbis_grcmanager/core.cljs
@@ -5,6 +5,7 @@
             [orbis-grcmanager.views :refer [main-page]]
             [orbis-grcmanager.pages.auth :refer [logged-in?]]
             [re-frame.core :refer [dispatch dispatch-sync]]
+            [re-frisk.core :as re-frisk]
     ;;initialize handlers and subscriptions
             orbis-grcmanager.handlers
             orbis-grcmanager.subscriptions))
@@ -14,6 +15,7 @@
 
 (defn init! []
   (dispatch-sync [:initialize-db])
+  (re-frisk/enable-re-frisk!)
   (if (logged-in?) (dispatch [:run-login-events]))
   (load-interceptors!)
   (hook-browser-navigation!)
